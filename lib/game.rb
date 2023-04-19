7c3d1393dfed539dc461aa0804c5114e4c35f13c
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-04-19 23:29:06 +0200

    drew a hangman

--- lib/game.rb
+++ lib/game.rb
@@ -18,14 +18,15 @@ include Function
 
 def winning
   puts
-  puts "And you found the right wors #{YELLOW}#{@hidden_word.join("")}#{ENDCOLOR}"
+  puts "The right word #{YELLOW}#{@hidden_word.join("")}#{ENDCOLOR}"
 
-  puts "The Poor Guy is alive! Such a relief! You are real hero. Do you want to play again?"
+  puts "The Poor Guy is alive! Such a relief! You are a real hero. Do you want to play again?"
 
   exit
 end
 
 def lost_game
+  self.hangman
   puts
   puts "You lost. Sorry, Mr. Poor Guy"
 
@@ -34,7 +35,7 @@ end
 
 def start_hidding
   @hidden_word.each{|letter|  " _ "}
-  puts "Nice to see you! We will play a hangman game. You need to guess the word hidden word before the Poor Guy will be hanged. You can make only 8 mistakes. Good luck to you and to the Poor Guy"
+  puts "Nice to see you! We will play a hangman game. You need to guess the hidden word before the Poor Guy will be hanged. You can make only 8 mistakes. Good luck to you and to the Poor Guy"
   puts "Word you need to guess:"
   @hidden_word.length.times { print "#{BLUE} _ #{ENDCOLOR}"}
   self.checking_guess
@@ -43,12 +44,16 @@ end
 def checking_guess
   while @wrong_letters.length != 8
     puts
+    self.hangman
+    puts
+    puts
     puts "Enter letter"
     letter = gets.chomp.downcase
     @hidden_word.include?(letter) ? @right_letters<<letter : @wrong_letters<<letter
 
     puts "#{8 - @wrong_letters.length} attempts left"
-    self.winning if @hidden_word.all?{|letter| @right_letters.include?(letter)}
+    puts "you have already tried: #{@wrong_letters.join(" ")}"
+
     @hidden_word.each do |symbol|
       if @right_letters.include?(symbol)
         print "#{BLUE} #{symbol} #{ENDCOLOR}"
@@ -56,6 +61,7 @@ def checking_guess
         print "#{BLUE} _ #{ENDCOLOR}"
       end
     end
+    self.winning if @hidden_word.all?{|letter| @right_letters.include?(letter)}
   end
   self.lost_game
 end
